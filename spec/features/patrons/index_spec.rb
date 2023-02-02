a72a5117f21446b201ddbadf368a74cb7bf3185e
Jade Stewart <jadekstewart3@gmail.com>
2023-02-02 15:27:30 -0700

    Fix testing to be in line with new table structure

--- spec/features/patrons/index_spec.rb
+++ spec/features/patrons/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'the patrons index page' do
   it 'displays the names of all of the patrons' do 
     gym = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
-    patron_1 = gym.patrons.create!(gym_id: 1, name: 'Jade Stewart', belay_certified: true)
-    patron_2 = gym.patrons.create!(gym_id: 1, name: 'Philip DeFraties', belay_certified: true)
+    patron_1 = gym.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
+    patron_2 = gym.patrons.create!(name: 'Philip DeFraties', years_member: 6, belay_certified: true)
     visit "/patrons"
 
     expect(page).to have_content(patron_1.name)
@@ -13,13 +13,13 @@ RSpec.describe 'the patrons index page' do
 
   it 'displays patrons attributes' do 
     gym = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
-    patron_1 = gym.patrons.create!(gym_id: 1, name: 'Jade Stewart', belay_certified: true)
-    patron_2 = gym.patrons.create!(gym_id: 1, name: 'Philip DeFraties', belay_certified: true)
+    patron_1 = gym.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
+    patron_2 = gym.patrons.create!(name: 'Philip DeFraties', years_member: 6, belay_certified: true)
     visit "/patrons"
 
-    expect(page).to have_content(patron_1.gym_id)
-    expect(page).to have_content(patron_2.gym_id)
-    expect(page).to have_content(patron_1.belay_certified)
-    expect(page).to have_content(patron_2.belay_certified)
+    expect(page).to have_content("Years a member: #{patron_1.years_member}")
+    expect(page).to have_content("Years a member: #{patron_2.years_member}")
+    expect(page).to have_content("Belay Certified: #{patron_1.belay_certified}")
+    expect(page).to have_content("Belay Certified: #{patron_2.belay_certified}")
   end
 end
--- spec/features/patrons/show_spec.rb
+++ spec/features/patrons/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.describe 'the patrons show page' do 
   it 'displays the name of the patron with associated id' do
     gym = ClimbingGym.create(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
-    patron_1 = gym.patrons.create!(gym_id: 1, name: 'Jade Stewart', belay_certified: true)
+    patron_1 = gym.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
     visit "/patrons/#{patron_1.id}"
 
     expect(page).to have_content(patron_1.name)
@@ -10,10 +10,10 @@ RSpec.describe 'the patrons show page' do
 
   it 'displays the patrons attributes' do 
     gym = ClimbingGym.create(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
-    patron_1 = gym.patrons.create!(gym_id: 1, name: 'Jade Stewart', belay_certified: true)
+    patron_1 = gym.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
     visit "/patrons/#{patron_1.id}"
 
-    expect(page).to have_content(patron_1.gym_id)
+    expect(page).to have_content(patron_1.years_member)
     expect(page).to have_content(patron_1.belay_certified)
   end
 end
