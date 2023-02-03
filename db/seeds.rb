7b4c755e58d390edddd093c29d6d1b69234dc242
Jade Stewart <jadekstewart3@gmail.com>
2023-02-02 19:42:22 -0700

    Add gym to seed data to create gyms created at differing times

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    @gym_1 = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
+    @gym_2 = ClimbingGym.create!(name: "Vertical Ventures", city: "Denver", number_of_routes: 300, classes_offered: false)
+    @gym_3 = ClimbingGym.create!(name: "Movement", city: "Englewood", number_of_routes: 1000, classes_offered: true)
+    @patron_1 = @gym_1.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
+    @patron_2 = @gym_1.patrons.create!(name: 'Philip DeFraties', years_member: 6, belay_certified: true)
+    @patron_3 = @gym_2.patrons.create!(name: 'Sean Morris', years_member: 7, belay_certified: true)
+    @patron_4 = @gym_2.patrons.create!(name: 'Denise Corona', years_member: 2, belay_certified: true)
