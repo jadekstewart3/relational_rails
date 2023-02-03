f4e6bfd271a99b371ba218f83573dff31774d30f
Jade Stewart <jadekstewart3@gmail.com>
2023-02-02 19:11:24 -0700

    Create sort model method and passing test

--- app/models/climbing_gym.rb
+++ app/models/climbing_gym.rb
@@ -1,3 +1,7 @@
 class ClimbingGym < ApplicationRecord
   has_many :patrons
+
+  def self.sort
+    ClimbingGym.order(created_at: :asc)
+  end
 end
--- spec/models/climbing_gym_spec.rb
+++ spec/models/climbing_gym_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe ClimbingGym do
   describe 'child relationship' do
     it {should have_many :patrons }
   end
+
+  describe '#sort' do 
+    it 'orders records by most recently created' do 
+      @gym_1 = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
+      @gym_2 = ClimbingGym.create!(name: "Vertical Ventures", city: "Denver", number_of_routes: 300, classes_offered: false)
+      @patron_1 = @gym_1.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
+      @patron_2 = @gym_1.patrons.create!(name: 'Philip DeFraties', years_member: 6, belay_certified: true)
+      @patron_3 = @gym_2.patrons.create!(name: 'Sean Morris', years_member: 7, belay_certified: true)
+      @patron_4 = @gym_2.patrons.create!(name: 'Denise Corona', years_member: 2, belay_certified: true)
+
+      expect(ClimbingGym.sort).to eq([@gym_1, @gym_2])
+    end
+  end
 end
+
+
+
+# User Story 6, Parent Index sorted by Most Recently Created 
+
+# As a visitor
+# When I visit the parent index,
+# I see that records are ordered by most recently created first
+# And next to each of the records I see when it was created
