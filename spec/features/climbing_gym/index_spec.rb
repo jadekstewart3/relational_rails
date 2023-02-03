5c23f9fd5d11f8bf2871e11f18ee1b8cae955f53
Jade Stewart <jadekstewart3@gmail.com>
2023-02-03 16:23:32 -0700

    Create tests for scope method

--- spec/features/climbing_gym/index_spec.rb
+++ spec/features/climbing_gym/index_spec.rb
@@ -21,4 +21,27 @@ RSpec.describe 'the climbing gyms index page' do
 
     expect(page).to have_content("Location: #{gym.city}")
   end
+
+  it 'displays the climbing gyms by most recently created' do
+    gym_1 = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true, created_at: Date.today)
+    gym_2 = ClimbingGym.create!(name: "Vertical Ventures", city: "Denver", number_of_routes: 300, classes_offered: false, created_at: Date.tomorrow)
+    gym_3 = ClimbingGym.create!(name: "EarthTreks", city: "Englewood", number_of_routes: 1000, classes_offered: true, created_at: Date.yesterday)
+    gym_4 = ClimbingGym.create!(name: "The Spot", city: "Denver", number_of_routes: 600, classes_offered: true)
+    visit "/climbing_gym"
+    displayed_gym_1 = page.find_by_id("gym-0").text
+    displayed_gym_2 = page.find_by_id("gym-1").text
+    displayed_gym_3 = page.find_by_id("gym-2").text
+    displayed_gym_4 = page.find_by_id("gym-3").text
+
+    expect(displayed_gym_1.include?(gym_2.name)).to be(true) 
+    expect(displayed_gym_2.include?(gym_4.name)).to be(true) 
+    expect(displayed_gym_3.include?(gym_1.name)).to be(true)
+    expect(displayed_gym_4.include?(gym_3.name)).to be(true)
+
+
+    expect("Vertical Ventures").to appear_before("The Spot", only_text: true)
+    expect("The Spot").to appear_before("Movement", only_text: true)
+    expect("Movement").to appear_before("EarthTreks", only_text: true)
+
+  end
 end
--- spec/features/climbing_gym/show_spec.rb
+++ spec/features/climbing_gym/show_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe 'the climbing gym show page' do
     expect(page).to have_content(gym.number_of_routes)
     expect(page).to have_content(gym.classes_offered)
   end
+
+  it 'displays the number of patrons that belong to the gym' do 
+    gym = ClimbingGym.create(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
+    patron_1 = gym.patrons.create!(name: 'Sean Morris', years_member: 7, belay_certified: true)
+    patron_2 = gym.patrons.create!(name: 'Brianna Campos', years_member: 2, belay_certified: true)
+    visit "/climbing_gym/#{gym.id}"
+
+    expect(page).to have_content()
+  end
+
+  # User Story 7, Parent Child Count
+
+  # As a visitor
+  # When I visit a parent's show page
+  # I see a count of the number of children associated with this parent
+
 end
