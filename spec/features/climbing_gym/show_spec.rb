99b5093e8623ecbf780793177225de08ec45bf7b
Jade Stewart <jadekstewart3@gmail.com>
2023-02-04 14:00:59 -0700

    Create test for link to climbing gyms patrons

--- spec/features/climbing_gym/show_spec.rb
+++ spec/features/climbing_gym/show_spec.rb
@@ -37,4 +37,15 @@ RSpec.describe 'the climbing gym show page' do
 
     expect(current_path).to eq("/climbing_gym")
   end
+
+  it 'links to its patrons' do 
+    gym = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
+    patron_1 = gym.patrons.create!(name: 'Sean Morris', years_member: 7, belay_certified: true)
+    patron_2 = gym.patrons.create!(name: 'Brianna Campos', years_member: 2, belay_certified: true)
+    visit  "/climbing_gym/#{gym.id}"
+
+    click_on "Gym Patrons"
+
+    expect(current_path).to eq("/climbing_gym/#{gym.id}/patrons")
+  end
 end
