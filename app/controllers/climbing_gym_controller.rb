6b0a12038fd060b9d5d5bd81dc853e8a62970b9e
Jade Stewart <jadekstewart3@gmail.com>
2023-02-04 18:01:20 -0700

    Create create method for creating a new climbing gym'

--- app/controllers/climbing_gym_controller.rb
+++ app/controllers/climbing_gym_controller.rb
@@ -7,4 +7,14 @@ class ClimbingGymController < ApplicationController
     @climbing_gym = ClimbingGym.find(params[:id])
     @patrons = @climbing_gym.patrons.count
   end
+
+  def new
+
+  end
+
+  def create
+    new_gym = ClimbingGym.create(name: params[:name], city: params[:city], number_of_routes: params[:number_of_routes], classes_offered: params[:classes_offered])
+
+    redirect_to "/climbing_gym"
+  end
 end
