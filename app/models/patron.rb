08f7a79eebba2c53af65aa30cacc152b7ae6c532
Jade Stewart <jadekstewart3@gmail.com>
2023-02-05 18:45:35 -0700

    Fix spaceing

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -2,6 +2,6 @@ class Patron < ApplicationRecord
   belongs_to :climbing_gym
 
   scope :is_belay_certified, -> { where("belay_certified") }
-
+  
   scope :sort_alphabetically, -> { order(name: :asc) }
 end
