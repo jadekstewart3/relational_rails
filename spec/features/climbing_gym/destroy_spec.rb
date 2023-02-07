cf535abd73a8c574ec60a8ba7848f8980132f9a1
Jade Stewart <jadekstewart3@gmail.com>
2023-02-06 19:25:51 -0700

    Create test for deletion of climbing gym on index page

--- spec/features/climbing_gym/destroy_spec.rb
+++ spec/features/climbing_gym/destroy_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe 'destroying a climbing gym' do
     expect(current_path).to eq("/climbing_gym")
     expect(page).to_not have_content("Vertical Ventures")
   end
+
+  it 'can delete a climbing gym from the index page'  do 
+    gym = ClimbingGym.create!(name: "Vertical Ventures", city: "Denver", number_of_routes: 600, classes_offered: true)
+
+    visit "/climbing_gym"
+
+    click_link "Delete #{gym.name}"
+    
+    expect(current_path).to eq("/climbing_gym")
+    expect(page).to_not have_content("Vertical Ventures")
+  end
 end
