008632c83610ed2de49a9c869b1ebb271524256b
Jade Stewart <jadekstewart3@gmail.com>
2023-02-07 16:04:28 -0700

    Add test for deleting a patron from the index page

--- spec/features/patrons/destroy_spec.rb
+++ spec/features/patrons/destroy_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe 'destroying a patron' do
     expect(current_path).to eq("/patrons")
     expect(page).to_not have_content("Philip DeFraties")
   end
+
+  it 'can delete a patron from the index page' do
+    gym = ClimbingGym.create!(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
+    patron_1 = gym.patrons.create!(name: 'Philip DeFraties', years_member: 6, belay_certified: true)
+    patron_2 = gym.patrons.create!(name: 'Jade Stewart', years_member: 6, belay_certified: true)
+
+    visit "/patrons"
+
+    click_link "Delete #{patron_1.name}"
+
+    expect(current_path).to eq("/patrons")
+    expect(page).to_not have_content("Philip DeFraties")
+    expect(page).to have_content("Jade Stewart")
+  end
 end
