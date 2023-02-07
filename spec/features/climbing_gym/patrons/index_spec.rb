a77ab115650ee8c76cc35899e5b760c97c8ec6f0
Jade Stewart <jadekstewart3@gmail.com>
2023-02-06 18:38:28 -0700

    Create test for displaying records over a given threshold

--- spec/features/climbing_gym/patrons/index_spec.rb
+++ spec/features/climbing_gym/patrons/index_spec.rb
@@ -59,4 +59,20 @@ RSpec.describe 'Climbing Gyms Patrons Index' do
       expect("Billie Jo McMurren").to appear_before("Jade Stewart")
     end
   end
+
+  describe 'display records over a given threshold' do
+    it 'dislays members who have been members over a certain number of years' do
+      gym = ClimbingGym.create(name: "Movement", city: "Golden", number_of_routes: 500, classes_offered: true)
+      patron_1 = gym.patrons.create!(name: 'Jade Stewart', years_member: 10, belay_certified: true)
+      patron_2 = gym.patrons.create!(name: 'Billie Jo McMurren', years_member: 6, belay_certified: true)
+
+      visit "climbing_gym/#{gym.id}/patrons"
+
+      fill_in 'Filter Members by Membership Length', with: 7
+      click_button 'Filter'
+
+      expect(current_path).to eq("/climbing_gym/#{gym.id}/patrons/filter")
+      expect(page).to_not have_content("Billie Jo McMurren")
+    end
+  end
 end
