b6648ca543531e34da8706778cc3abe0d56b622d
Jade Stewart <jadekstewart3@gmail.com>
2023-02-06 17:15:44 -0700

    Add destroy method for patrons

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -17,6 +17,12 @@ class PatronsController < ApplicationController
     redirect_to "/patrons/#{patron.id}"
   end
 
+  def destroy 
+    patron = Patron.find(params[:id])
+    patron.destroy
+    redirect_to '/patrons'
+  end
+
   private
   def patron_params
     params.permit(:name, :years_member, :belay_certified)
